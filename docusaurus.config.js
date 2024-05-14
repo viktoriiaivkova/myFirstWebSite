d89164429a34d051aae649793bc6f19992b6b788
viktoriiaivkova <jesuspristine@gmail.com>
2024-05-14 21:12:04 +0300

    changed logo and background.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,9 +66,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Pigs',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Piggy logo..',
           src: 'img/logo.svg',
         },
         items: [
